275a5fdcb20c1c55601c0c9f771b5a8796fa32fe
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2020-07-28 15:34:55 +0300

    Add files via upload

--- amazon/js/script.js
+++ amazon/js/script.js
@@ -8,7 +8,7 @@
       menu.classList.toggle('active');
     };
 
-
+//ACCORDION//
     let acc = document.querySelectorAll(".questions__content");
 
     acc.forEach(function (item, i, arr) {
@@ -36,10 +36,57 @@
         } 
     }
 
-    // let plus = document.querySelectorAll(".questions__plus");
+//COOKIE//
+    
+let buttonCookie = document.querySelectorAll('.cookie__buttons > button');
+    let cookie = document.querySelector('.cookie');
 
-    // for(i=0, i<arrow.length, i++) {
-    //     plus[i].addEventListener('click', function(){
+    buttonCookie[0].addEventListener('click', function(){
+        cookie.classList.add('hide');
+    });
 
-    // });
-    // }
+    buttonCookie[1].addEventListener('click', function(){
+        cookie.classList.add('hide');
+    });
+
+//MENU MOBILE//
+
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+        let body = document.querySelector('body');
+        
+if (isMobile.any()){
+        body.classList.add('touch');
+
+        // let link = document.querySelectorAll('.menu__link');
+        // let i;
+
+        // for(i = 0; i < link.length; i++){
+        //     link[i].addEventListener('click', function(){
+        //         this.classList.toggle('active');
+        //         let submenu = this.nextElementSibling;
+        //         submenu.classList.add('active');
+        //     });
+        // }
+        // let arrow = document.querySelectorAll('.menu__arrow');
+        // const target = arrow.target;
+
+        // arrow.forEach(function(item, i, arrow) {
+        //     item.addEventListener('click', function(item, i){
+        //         if (target == item){
+        //             arrow.classList.toggle('active');
+        //         } else {
+        //             console.log('jjhh');
+        //         }
+        //     });
+        // });
+      
+} else {
+    body.classList.add('mouse');
+}
