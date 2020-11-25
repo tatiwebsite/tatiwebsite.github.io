6b34f7f609357311e61e3bc1879ad1c2bdfdf4cb
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2020-11-25 16:26:09 +0300

    Add files via upload

--- brandallica/js/script.js
+++ brandallica/js/script.js
@@ -1,39 +1,30 @@
+window.addEventListener('DOMContentLoaded', function(){
 
-//BURGER
-    let burger = document.querySelector('.menu-mobile');
-    let menu = document.querySelector('.menu');
+    const agreeCookieMob = document.querySelector('.cookies-mob__agree'),
+          notCookieMob = document.querySelector('.cookies-mob__not'),
+          cookiesMob = document.querySelector('.cookies-mob'),
+          agreeCookieDesk = document.querySelector('.cookies-desk__agree'),
+          notCookieDesk = document.querySelector('.cookies-desk__not'),
+          cookiesDesk = document.querySelector('.cookies-desk');
 
-    burger.onclick = function() {
-      burger.classList.toggle('active');
-      menu.classList.toggle('active');
-    };
-
-//ACCORDION//
-    let acc = document.querySelectorAll(".questions__content");
-
-    acc.forEach(function (item, i, arr) {
-        item.addEventListener("click", function() {
-            arr.forEach(function(q) {
-                if (q !== item) {
-                    toggleAcc(q, true);
-                }
-            });
-
-            toggleAcc(item);
-        });
+    agreeCookieMob.addEventListener('click', function(){
+        cookiesMob.classList.add('cookies-mob_hide');
+        cookiesMob.classList.add('cookies-mob_hide');
     });
 
-    function toggleAcc(tab, closeOnly) {
-        let answer = tab.nextElementSibling;
-        let plus = tab.lastElementChild;
+    notCookieMob.addEventListener('click', function(){
+        cookiesMob.classList.add('cookies-mob_hide');
+    });
 
-        if (answer.style.maxHeight && answer.style.maxHeight != "0px" || closeOnly){
-            answer.style.maxHeight = 0;
-            plus.style.color = '#4285f0';
-        } else {
-            answer.style.maxHeight = answer.scrollHeight + "px";
-            plus.style.color = '#ea4330';
-        } 
-    }
+    agreeCookieDesk.addEventListener('click', function(){
+        cookiesDesk.classList.add('cookies-desk_hide');
+        cookiesDesk.classList.add('cookies-desk_hide');
+    });
 
-    
+    notCookieDesk.addEventListener('click', function(){
+        cookiesDesk.classList.add('cookies-desk_hide');
+    });
+
+
+
+});
