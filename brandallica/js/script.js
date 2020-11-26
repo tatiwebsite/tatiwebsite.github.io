50a76c6643e001fa13b2e58c42b311ac16eabda4
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2020-11-26 17:47:42 +0300

    Add files via upload

--- brandallica/js/script.js
+++ brandallica/js/script.js
@@ -1,43 +1,57 @@
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+                        .split(';')
+                        .map(cookie => cookie.split('='))
+                        .reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
+        return cookies[key];
+    },
+    setItem: function(key, value) {
+        document.cookie = `${key}=${value}; expires=Sun, 16 Jul 3567 06:23:41 GMT`;
+    }
+};
+
+
+const storageType = cookieStorage;
+// const storageType = sessionStorage;
+const consentPropertyType = 'site_consent';
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyType);
+const saveToStorage = () => storageType.setItem(consentPropertyType, true);
+
 window.addEventListener('DOMContentLoaded', function(){
 
-    const agreeCookieMob = document.querySelector('.cookies-mob__agree'),
-          notCookieMob = document.querySelector('.cookies-mob__not'),
-          cookiesMob = document.querySelector('.cookies-mob'),
-          agreeCookieDesk = document.querySelector('.cookies-desk__agree'),
-          notCookieDesk = document.querySelector('.cookies-desk__not'),
-          cookiesDesk = document.querySelector('.cookies-desk'),
+//Cookies
+
+
+
+    const popupCookie = document.querySelector('.cookies'),
+          cookieAgree = document.querySelector('.cookies__agree'),
           ticker = document.querySelector('.footer__animate');
 
-    agreeCookieMob.addEventListener('click', function(){
-        cookiesMob.classList.add('cookies-mob_hide');
-        cookiesMob.classList.add('cookies-mob_hide');
-    });
+    if (shouldShowPopup()) {
+        popupCookie.classList.add('cookies_active');
+    }
 
-    notCookieMob.addEventListener('click', function(){
-        cookiesMob.classList.add('cookies-mob_hide');
+    cookieAgree.addEventListener('click', () => {
+        saveToStorage();
+        popupCookie.classList.remove('cookies_active');
     });
-
-    agreeCookieDesk.addEventListener('click', function(){
-        cookiesDesk.classList.add('cookies-desk_hide');
-        cookiesDesk.classList.add('cookies-desk_hide');
-    });
-
-    notCookieDesk.addEventListener('click', function(){
-        cookiesDesk.classList.add('cookies-desk_hide');
-    });
+    
 
 //AnimateTicker
-    function animateTicker() {
-        ticker.classList.add('footer__animate_on');
-    }
-    
-    function animateTickerByScroll () {
-        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            animateTicker();
-            window.removeEventListener('scroll', animateTickerByScroll); 
-        }
-    }
+function animateTicker() {
+    ticker.classList.add('footer__animate_on');
+}
 
-    window.addEventListener('scroll', animateTickerByScroll);
+function animateTickerByScroll () {
+    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 20) {
+        animateTicker();
+        window.removeEventListener('scroll', animateTickerByScroll); 
+    }
+}
+
+window.addEventListener('scroll', animateTickerByScroll);
 
 });
+
