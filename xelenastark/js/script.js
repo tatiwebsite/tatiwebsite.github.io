a54ff09b1463e84eb7909c4a907f503488dbb0e7
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2020-12-16 02:07:46 +0300

    Add files via upload

--- xelenastark/js/script.js
+++ xelenastark/js/script.js
@@ -33,27 +33,68 @@ new Swiper('.reviews-slider', {
     watchSlidesVisibility: true,
 });
 
-//Плавный скролл//
 
+const popup = document.querySelector('.pop-up'),
+      overlay = document.querySelector('.overlay'),
+      openModal = document.querySelectorAll('[data-modal=buy]'),
+      closeModal = document.querySelector('.pop-up__close');
+ 
+function modalOpenBtn () {
+    overlay.classList.add('show');
+    overlay.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+}      
+
+openModal.forEach(btn => {
+    btn.addEventListener('click', modalOpenBtn);
+});
+
+function modalCloseBtn () {
+    overlay.classList.remove('show');
+    overlay.classList.add('hide');
+    document.body.style.overflow = '';
+}
+
+closeModal.addEventListener('click', modalCloseBtn);
+
+overlay.addEventListener('click', (e) => {
+    if(e.target === overlay) {
+        modalCloseBtn();
+    }
+});
+
+//Плавный скролл//
+$(document).ready(function() {
+  
 $("a[href^='#']").click(function(){
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
   }); 
 
-  function validateForms (form) {
+function validateForms (form) {
     $(form).validate({
-      rules: {
-        name: "required",
-        email: {
-          required: true,
-          email: true
-        }
-      }
+        rules: {
+            name: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+                },
+            email: {
+                required: true,
+                email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите ваше имя",
+                email: {
+                required: "Пожалуйста, введите адрес почтового ящика",
+                email: "Адрес почтового ящика введен неверно"
+                }
+            }
     });
-  }
-
-  validateForms('form');
-  
-
+}
+    validateForms('#form');
+    validateForms('#popup-form');
+});
     
