c58657bb046b59e79142ca813a90cfe481572553
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2020-12-31 00:59:43 +0300

    Add files via upload

--- inkognito/js/script.js
+++ inkognito/js/script.js
@@ -11,4 +11,19 @@ $(document).ready(function() {
         $('.hamburger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+
+    $('.services__button').click(function(){
+        $(this).next('.overlay').fadeIn('slow');
+        $('body').toggleClass('lock');
+    });
+
+    
+
+    $('.overlay').click(function(e) { 
+        if($(e.target).closest(".popup").length==0) $('.overlay').fadeOut('slow');
+      });
+
+    $('.popup__close').click(function(){
+        $('.overlay').fadeOut('slow');
+    });
 });
