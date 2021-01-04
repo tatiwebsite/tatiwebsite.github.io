b7e4332703e51f05fcfd31ff086cae7d6a579144
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2021-01-04 23:14:24 +0300

    Add files via upload

--- inkognito/js/script.js
+++ inkognito/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    //Аккордеон
     $('.portfolio__case').click(function(){
         $(this).next().slideToggle(200);
     });
@@ -8,49 +7,23 @@ $(document).ready(function() {
         $(this).next().slideToggle(200);
     });
 
-    //Бургер
     $('.hamburger').click(function(event){
         $('.hamburger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
 
-    //Открываем и закрываем все окна, кроме формы
-    $('.open-popup').click(function(){
+    $('.services__button').click(function(){
         $(this).next('.overlay').fadeIn('slow');
-        $('body').addClass('lock');
+        $('body').toggleClass('lock');
     });
+
+    
 
     $('.overlay').click(function(e) { 
         if($(e.target).closest(".popup").length==0) $('.overlay').fadeOut('slow');
-        $('body').removeClass('lock');
       });
 
     $('.popup__close').click(function(){
         $('.overlay').fadeOut('slow');
-        $('body').removeClass('lock');
     });
-
-    //Открываем и закрываем форму
-
-    $('.prices__button').click(function(){
-        $('#overlay-form').fadeIn('slow');
-        $('body').addClass('lock');
-    });
-
-    $('.overlay-form').click(function(e) { 
-        if($(e.target).closest(".popup-form").length==0) $('.overlay-form').fadeOut('slow');
-        $('body').removeClass('lock');
-      });
-
-    $('.popup-form__close').click(function(){
-        $('.overlay-form').fadeOut('slow');
-        $('body').removeClass('lock');
-    });
-
-    //Плавный скролл
-    $("a[href^='#']").click(function(){
-        const _href = $(this).attr("href");
-        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-        return false;
-      }); 
 });
