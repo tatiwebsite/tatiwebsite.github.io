a7bd62adaad17e430ce868b824712c7335318988
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2021-04-13 09:34:55 +0300

    Add files via upload

--- perfomante/js/script.js
+++ perfomante/js/script.js
@@ -1,66 +1,68 @@
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    // var coords = document.documentElement.clientHeight;
+
+    if (scrolled > 1000) {
+      goTopBtn.classList.add('up-show');
+    }
+    if (scrolled < 1000) {
+      goTopBtn.classList.remove('up-show');
+    }
+  }
+
+  function up() {
+    if (window.pageYOffset > 0) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.up');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', up);
+})();
+
+//BURGER
+
+let burger = document.querySelector('.mobile-menu');
+let menu = document.querySelector('.menu');
+
+
+burger.addEventListener('click', function() {
+  burger.classList.toggle('active');
+  menu.classList.toggle('active');
+  document.body.classList.toggle('active');
+});
+
+  
+  // burger.classList.toggle('active');
+  // menu.classList.toggle('active');
+  // document.body.style.overflow = 'hidden';
 
 
 new Swiper('.interviews-slider', {
-    // // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-  
-    // // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      //Булеты
       clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
-    slidesPerView: 2.5,
-    spaceBetween: 10,  
+    spaceBetween: 20,  
     loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
 
     breakpoints: {
       320: {
-        slidesPerView: 1.2,
-      },
-      400: {
-        slidesPerView: 1.4,
-      },
-      576: {
-        slidesPerView: 1.2,
+        spaceBetween: 20,
       },
       768: {
-        slidesPerView: 1.4,
+        spaceBetween: 28,
       },
-      991: {
-        slidesPerView: 1.6,
-      },
-      1200: {
-        slidesPerView: 1.9,
-        spaceBetween: 20,
-
-      },
-      1500: {
-        slidesPerView: 2.5,
-      },
-      1900: {
-        slidesPerView: 3.1,
-      },
-      2200: {
-        slidesPerView: 3.7,
-      },
-      2800: {
-        slidesPerView: 4.2,
-      },
-      3500: {
-        slidesPerView: 5.2,
-      },
-      4000: {
-        slidesPerView: 6.2,
-      },
-
-    },
+    }
 });
