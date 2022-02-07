fa6d1cae1667e25258962e96ba3ebcab5b17236c
tatiwebsite <62358093+tatiwebsite@users.noreply.github.com>
2022-02-08 00:29:07 +0300

    Add files via upload

--- noesNew/js/script.js
+++ noesNew/js/script.js
@@ -43,7 +43,6 @@ const swiperNext = document.getElementById('swiperNext');
 
 if(swiperPrev && swiperNext){
   swiperPrev.addEventListener('click', () => {
-    console.log('click');
     mySwiper.slidePrev();
     });
     
@@ -63,137 +62,131 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   
 
-    // let locWindow = window.location;
+  let locWindow = window.location.pathname.slice(1);
+  
+  let linksNav = document.querySelectorAll('.menu__link');
+  linksNav.forEach(link => {
+    let linkData = link.getAttribute('data-path');
+    if(locWindow == link.dataset.path){
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+  
     
-    // let linksNav = document.querySelectorAll('.menu__link');
-    // linksNav.forEach(link => {
-    //   let linkData = link.getAttribute('data-path');
-    //   if(locWindow == link.dataset.path){
-    //     link.classList.add('active');
-    //   } else {
-    //     link.classList.remove('active');
-    //   }
-    // });
-    
-    // if(item.innerText.toLowerCase().search(val.toLowerCase()) == -1) 
-    //   window.addEventListener('hashchange', () => {
-    //     if(!shouldShowLogin()){
-    //         let id = window.location.hash;
-    //         showPage(id);
-    //     } else {
-    //         window.location.hash = '#login-page';
-    //         showPage('#login-page');
-    //     }    
-    // });
-    
-      //Burger
-      let burger = document.querySelector('.burger'),
-          menu = document.querySelector('.menu'),
-          menuClose = document.querySelector('.menu__close'),
-          body = document.querySelector('body');
-    
-      burger.addEventListener('click', () => {
-        menu.classList.add('active');
-        body.classList.add('locked');
-      });
-    
-      menuClose.addEventListener('click', () => {
-        menu.classList.remove('active');
-        body.classList.remove('locked');
-      });
-    
-      //Accordeon
-      let acc = document.querySelectorAll(".questions__content");
-        acc.forEach(function (item, i, arr) {
-            item.addEventListener("click", function() {
+  //Burger
+  let burger = document.querySelector('.burger'),
+      menu = document.querySelector('.menu'),
+      menuClose = document.querySelector('.menu__close'),
+      body = document.querySelector('body');
 
-                arr.forEach(function(q) {
-                    if (q !== item) {
-                        toggleAcc(q, true);
-                    }
-                });
-    
-                toggleAcc(item);
-            });
-        });
-    
-        function toggleAcc(tab, closeOnly) {
-          let answer = tab.nextElementSibling;
-          let plus = tab.lastElementChild;
-          if (answer.style.maxHeight && answer.style.maxHeight != "0px" || closeOnly){
-            answer.style.maxHeight = 0;
-            plus.classList.remove('active');
-          } else {           
-            answer.style.maxHeight = answer.scrollHeight + "px";
-            plus.classList.add('active');
-          } 
-        }
-        
-        //Открыть больше в admission
-        let showMore = document.querySelectorAll(".show-more");
+  burger.addEventListener('click', () => {
+    openOverlay(menu);
+  });
 
-        showMore.forEach(btn => {
-          btn.addEventListener('click', () => {
-            let hideAnswer = btn.nextElementSibling;
-            let admissionBlocks = document.querySelectorAll('.admission__inner');
+  menuClose.addEventListener('click', () => {
+    closeOverlay(menu);
+  });
 
-            if(!btn.classList.contains('active') && hideAnswer.classList.contains('hide')){
-              btn.classList.add('active');
-              hideAnswer.style.maxHeight = hideAnswer.scrollHeight + "px";
-              admissionBlocks.forEach(item => {
-                item.classList.add('short');
-              });
-            } else if (btn.classList.contains('active') && hideAnswer){
-              admissionBlocks.forEach(item => {
-                setTimeout(() => item.classList.remove('short'), 298);
-              });
-              hideAnswer.style.maxHeight = '0';
-              btn.classList.remove('active');
+  //Accordeon
+  let acc = document.querySelectorAll(".questions__content");
+  acc.forEach(function (item, i, arr) {
+    item.addEventListener("click", function() {
+
+        arr.forEach(function(q) {
+            if (q !== item) {
+                toggleAcc(q, true);
             }
-          });
         });
 
-        //Открыть больше training
-        let programmsWrapper = document.querySelectorAll('.programms__block');
-        programmsWrapper.forEach(item => {
-          item.addEventListener('click', (e) => {
-            if(e.target && e.target.classList.contains('programms__open')){
-              let btn = e.target,
-                  list = item.querySelector('.programms__list');
-              if(!btn.classList.contains('active')){
-                btn.classList.add('active');
-                list.classList.remove('hide');
-              } else {
-                btn.classList.remove('active');
-                list.classList.add('hide');
-              }
-            }
-          });
-        });
-        
-    
+        toggleAcc(item);
+    });
+  });
 
-        //Увеличиваем изображения
-        const scaleWrapper = document.querySelectorAll('.scale-img-wrapper');
-    
-        if(scaleWrapper){
-          scaleWrapper.forEach(item => {
-            item.addEventListener('click', (e) => {
-              if(e.target && e.target.classList.contains('scale-img')){
-                if(e.target.classList.contains('scale')){
-                  e.target.classList.remove('scale')
-                } else {
-                  let imgIncreased = item.querySelector('.scale-img.scale');
-                    if(imgIncreased ) {
-                      imgIncreased.classList.remove('scale');
-                    }
-                  e.target.classList.add('scale');
-                }
-              }
-            });
-          })
+  function toggleAcc(tab, closeOnly) {
+    let answer = tab.nextElementSibling;
+    let plus = tab.lastElementChild;
+    if (answer.style.maxHeight && answer.style.maxHeight != "0px" || closeOnly){
+      answer.style.maxHeight = 0;
+      plus.classList.remove('active');
+    } else {           
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      plus.classList.add('active');
+    } 
+  }
+  
+  //Открыть больше в admission
+  let showMore = document.querySelectorAll(".show-more");
+
+  showMore.forEach(btn => {
+    btn.addEventListener('click', () => {
+      let hideAnswer = btn.nextElementSibling;
+      let admissionBlocks = document.querySelectorAll('.admission__inner');
+
+      if(!btn.classList.contains('active') && hideAnswer.classList.contains('hide')){
+        btn.classList.add('active');
+        hideAnswer.style.maxHeight = hideAnswer.scrollHeight + "px";
+        admissionBlocks.forEach(item => {
+          item.classList.add('short');
+        });
+      } else if (btn.classList.contains('active') && hideAnswer){
+        admissionBlocks.forEach(item => {
+          setTimeout(() => item.classList.remove('short'), 298);
+        });
+        hideAnswer.style.maxHeight = '0';
+        btn.classList.remove('active');
+      }
+    });
+  });
+
+  //Открыть больше training
+  let programmsWrapper = document.querySelectorAll('.programms__block');
+  programmsWrapper.forEach(item => {
+    item.addEventListener('click', (e) => {
+      if(e.target && e.target.classList.contains('programms__open')){
+        let btn = e.target,
+            list = item.querySelector('.programms__list');
+        if(!btn.classList.contains('active')){
+          btn.classList.add('active');
+          list.classList.remove('hide');
+        } else {
+          btn.classList.remove('active');
+          list.classList.add('hide');
         }
+      }
+    });
+  });
 
+  //Увеличиваем изображения
+  const scaleWrapper = document.querySelectorAll('.scale-img-wrapper');
+
+  if(scaleWrapper){
+    scaleWrapper.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if(e.target && e.target.classList.contains('scale-img')){
+          if(e.target.classList.contains('scale')){
+            e.target.classList.remove('scale')
+          } else {
+            let imgIncreased = item.querySelector('.scale-img.scale');
+              if(imgIncreased ) {
+                imgIncreased.classList.remove('scale');
+              }
+            e.target.classList.add('scale');
+          }
+        }
+      });
+    })
+  }
+
+
+        // var input = document.querySelector("#phone");
+        // intlTelInput(input, {
+        //   utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.15/js/intlTelInput.min.js",
+        //   customPlaceholder : function ( selectedCountryPlaceholder ,  selectedCountryData )  { 
+        //     return  "eg"  +  selectedCountryPlaceholder ; 
+        //   }
+        // });
 
         // //Записаться на программу
         // const orderBtn = document.querySelector('.tab-program__btn'),
@@ -235,55 +228,40 @@ window.addEventListener('DOMContentLoaded', () => {
         // }  
     
         //Оставить заявку
-        const enrollPopup = document.querySelector('[data-set="enroll"]'),
+        const enrollPopup = document.querySelector('[data-set="enroll-overlay"]'),
               enrollBtn = document.querySelectorAll('.to-enroll'),
-              callPopup = document.querySelectorAll('[data-set="call"]');
+              callBtn = document.querySelectorAll('.to-call'),
+              callPopup = document.querySelector('[data-set="call-overlay"]'),
+              closePopup = document.querySelectorAll('.popup__close');
     
         enrollBtn.forEach((item) => {
-          item.addEventListener('click', openOverlay());
+          item.addEventListener('click', () => {
+            openOverlay(enrollPopup);
+          })
+        });
+
+        callBtn.forEach((item) => {
+          item.addEventListener('click', (e) => {
+            e.preventDefault();
+            openOverlay(callPopup);
+          })
+        });
+
+        closePopup.forEach((item) => {
+          item.addEventListener('click', () => {
+            let thisPopup = item.closest('.overlay-popup');
+            closeOverlay(thisPopup);
+          })
         });
     
-        function openOverlay(){
-          callModal.classList.add('active');
+        function openOverlay(item){
+          item.classList.add('active');
           body.classList.add('locked');
         }
 
-        function closeOverlay(){
-          callModal.classList.remove('active');
+        function closeOverlay(item){
+          item.classList.remove('active');
           body.classList.remove('locked');
         }
-    
-        callModal.querySelector('.order-modal__close').addEventListener('click', () => {
-          callModal.classList.remove('opened');
-          body.classList.remove('locked');
-        });
-    
-        // //Поиск программы
-        // const searchInput = document.querySelector('.search');
-        // if(searchInput){
-        //   searchInput.addEventListener('input', function(){
-        //     let val = this.value.trim();
-        //     let activeTab = document.querySelector('.tabcontent.show'),
-        //         programms = activeTab.querySelectorAll('.tab-program__title');
-        
-        //     if(val != ''){
-        //       programms.forEach((item) => {
-        //         if(item.innerText.toLowerCase().search(val.toLowerCase()) == -1) {
-        //           let itemParent = item.closest('.tab-program');
-        //           itemParent.classList.add('hide');
-        //         } else {
-        //           let itemParent = item.closest('.tab-program');
-        //           itemParent.classList.remove('hide');
-        //         }
-        //       });
-        //     }
-        //     else {
-        //       programms.forEach((item) => {
-        //         item.classList.remove('hide');
-        //       });
-        //     }
-        //   }); 
-        // }
-        
         
     });
